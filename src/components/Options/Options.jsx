52665136dc7992719c29276602b25b5aed56bc93
Serhii Vasiliev <sepovi4@gmail.com>
2024-05-05 20:23:11 +0200

    Update button "reset"

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,9 +1,13 @@
-export default function Options({ updateFeedback }) {
+export default function Options({ updateFeedback, totalFeedback, setValues }) {
+  const handleReset = () => {
+    setValues({ good: 0, neutral: 0, bad: 0 });
+  };
   return (
     <>
       <button onClick={() => updateFeedback("good")}>Good</button>
       <button onClick={() => updateFeedback("neutral")}>Neutral</button>
       <button onClick={() => updateFeedback("bad")}>Bad</button>
+      {totalFeedback > 0 && <button onClick={handleReset}>Reset</button>}
     </>
   );
 }
